4477ce8cce20380e1df8472b03e8493fec623c50
prashantkrishna5 <prashantkrishna5@github.com>
2023-08-10 17:47:08 +0530

    java-installer-ubuntu

---
+++ install-java.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+printf "installing jdk....";
+sudo apt install default-jdk;
+printf "JDK has been is successfully installed. Installing JRE...";
+sudo apt install default-jre;
+printf "JRE has been installed"
+
